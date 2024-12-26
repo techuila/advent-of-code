b517d550c32d6b3a2ccb540c2690bf435bb08e9d
techuila <axlcuyugan05@gmail.com>
2024-12-26 14:54:55 +0800

    solved Day 02 2024 (part 2) | typescript

--- typescript/src/2024/02_red_nosed_reports/index.test.ts
+++ typescript/src/2024/02_red_nosed_reports/index.test.ts
@@ -13,6 +13,14 @@ describe("Tests for Advent of code 2024, day 2: Red Nosed Reports", () => {
 
       expect(result).toEqual(2);
     });
+
+    test("Test from example 1, it should return 4", () => {
+      const inputs = get_inputs("example1.txt");
+
+      const result = redNosedReports.solution_2(inputs);
+
+      expect(result).toEqual(4);
+    });
   });
 
   describe("Test Solutions", () => {
@@ -20,15 +28,14 @@ describe("Tests for Advent of code 2024, day 2: Red Nosed Reports", () => {
       const inputs = get_inputs("inputs.txt");
 
       const result = redNosedReports.solution_1(inputs);
-      console.log(result);
       expect(result).toEqual(591);
     });
 
-    //test("Test from solution 2, it should return 67_363", () => {
-    //  const inputs = get_inputs("inputs.txt");
-    //
-    //  const result = redNosedReports.solution_2(inputs);
-    //  console.log(result);
-    //});
+    test("Test from solution 2, it should return 621", () => {
+      const inputs = get_inputs("inputs.txt");
+
+      const result = redNosedReports.solution_2(inputs);
+      expect(result).toEqual(621);
+    });
   });
 });
--- typescript/src/2024/02_red_nosed_reports/index.ts
+++ typescript/src/2024/02_red_nosed_reports/index.ts
@@ -36,7 +36,45 @@ export class RedNosedReports implements AOC {
     return total_safe;
   }
 
-  solution_2(inputs: string[]) {}
+  solution_2(inputs: string[]) {
+    function verify(input_arr: number[], exclude_index?: number): number {
+      let nums = [...input_arr];
+      if (exclude_index !== undefined) {
+        nums.splice(exclude_index, 1);
+      }
+
+      const is_increasing = nums[1] - nums[0] > 0;
+      let is_safe = true;
+
+      for (let i = 1; i < nums.length; i++) {
+        const diff = nums[i] - nums[i - 1];
+
+        if ((is_increasing && diff <= 0) || Math.abs(diff) > 3) {
+          is_safe = false;
+          break;
+        }
+
+        if ((!is_increasing && diff >= 0) || Math.abs(diff) > 3) {
+          is_safe = false;
+          break;
+        }
+      }
+
+      if (!is_safe && exclude_index === undefined) {
+        nums.forEach((_, index) => {
+          is_safe ||= Boolean(verify(input_arr, index));
+        });
+      }
+
+      return is_safe ? 1 : 0;
+    }
+
+    return inputs.reduce(
+      (total_safe, input) =>
+        (total_safe += verify(input.split(" ").map(Number))),
+      0,
+    );
+  }
 
   checkLevel = (
     input_arr: number[],
