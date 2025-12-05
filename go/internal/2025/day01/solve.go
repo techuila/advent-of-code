2e5a18eb2569bc72455da2df3ca510385d8b9e06
techuila <axlcuyugan05@gmail.com>
2025-12-06 00:25:19 +0800

    🎉 solved Day 01 2025 (part 2) | go

--- go/internal/2025/day01/solve.go
+++ go/internal/2025/day01/solve.go
@@ -10,23 +10,74 @@ func SolvePart1(input []string) int {
 	var answer int
 
 	for _, rotation := range input {
-		var dir int
-		l_r := rotation[0]
-		turns, _ := strconv.Atoi(rotation[1:])
+		dir, turns := getDirTurns(rotation)
 
-		if l_r == 'L' {
-			dir = -1
-		} else {
-			dir = 1
-		}
+		dialPointer := (100 + (start+dir*turns)%100) % 100
+		start = dialPointer
 
-		dial_pointer := (100 + (start+dir*turns)%100) % 100
-		start = dial_pointer
-
-		if dial_pointer == 0 {
+		if dialPointer == 0 {
 			answer++
 		}
 	}
 
 	return answer
+}
+
+func SolvePart2(input []string) int {
+	var start = 50
+	var answer int
+
+	for _, rotation := range input {
+		dir, turns := getDirTurns(rotation)
+
+		dialPointer := getDialPosition(start, dir, turns)
+		zerosPassed := countZerosPassed(start, dir, turns)
+
+		if zerosPassed >= 1 {
+			answer += zerosPassed
+		}
+
+		start = dialPointer
+	}
+
+	return answer
+}
+
+func countZerosPassed(start int, dir int, turns int) int {
+	var stepsToZero int
+
+	if dir > 0 { // Right direction
+		stepsToZero = (100 - start)
+	} else { // Left direction
+		stepsToZero = start
+	}
+
+	if stepsToZero == 0 {
+		stepsToZero = 100
+	}
+
+	if turns < stepsToZero {
+		return 0
+	}
+
+	return 1 + (turns-stepsToZero)/100
+}
+
+func getDialPosition(start int, dir int, turns int) int {
+	dirTurns := dir * turns
+	return (100 + (start+dirTurns)%100) % 100
+}
+
+func getDirTurns(rotation string) (int, int) {
+	var dir int
+	lr := rotation[0]
+	turns, _ := strconv.Atoi(rotation[1:])
+
+	if lr == 'L' {
+		dir = -1
+	} else {
+		dir = 1
+	}
+
+	return dir, turns
 }
--- go/internal/2025/day01/solve_test.go
+++ go/internal/2025/day01/solve_test.go
@@ -9,12 +9,18 @@ import (
 func TestSolveExample(t *testing.T) {
 	data, _ := os.ReadFile("testdata/example.txt")
 
-	result := SolvePart1(strings.Split(string(data), "\n"))
+	result1 := SolvePart1(strings.Split(string(data), "\n"))
+	result2 := SolvePart2(strings.Split(string(data), "\n"))
 
-	expected := int(3)
+	expected1 := int(3)
+	expected2 := int(6)
 
-	if result != expected {
-		t.Errorf("Expected %d but got %d", expected, result)
+	if result1 != expected1 {
+		t.Errorf("[Example 1] Expected %d but got %d", expected1, result1)
+	}
+
+	if result2 != expected2 {
+		t.Errorf("[Example 2] Expected %d but got %d", expected2, result2)
 	}
 }
 
@@ -24,6 +30,18 @@ func TestSolvePart1(t *testing.T) {
 	result := SolvePart1(strings.Split(string(data), "\n"))
 
 	expected := int(1172)
+
+	if result != expected {
+		t.Errorf("Expected %d but got %d", expected, result)
+	}
+}
+
+func TestSolvePart2(t *testing.T) {
+	data, _ := os.ReadFile("testdata/input_2.txt")
+
+	result := SolvePart2(strings.Split(string(data), "\n"))
+
+	expected := int(6932)
 
 	if result != expected {
 		t.Errorf("Expected %d but got %d", expected, result)
