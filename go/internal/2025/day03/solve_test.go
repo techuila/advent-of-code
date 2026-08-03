bedcf23f781f6ce83e2c16202b4867f0af60890a
techuila <axlcuyugan05@gmail.com>
2026-08-03 23:55:43 +0800

    uncomment tests

--- go/internal/2025/day03/solve_test.go
+++ go/internal/2025/day03/solve_test.go
@@ -10,18 +10,18 @@ func TestSolveExample(t *testing.T) {
 	data, _ := os.ReadFile("testdata/example.txt")
 
 	result1 := SolvePart1(strings.Split(string(data), "\n"))
-	// result2 := SolvePart2(strings.Split(string(data), "\n"))
+	result2 := SolvePart2(strings.Split(string(data), "\n"))
 
 	expected1 := int(357)
-	// expected2 := int()
+	expected2 := int()
 
 	if result1 != expected1 {
 		t.Errorf("[Example 1] Expected %d but got %d", expected1, result1)
 	}
 
-	// if result2 != expected2 {
-	// 	t.Errorf("[Example 2] Expected %d but got %d", expected2, result2)
-	// }
+	if result2 != expected2 {
+		t.Errorf("[Example 2] Expected %d but got %d", expected2, result2)
+	}
 }
 
 func TestSolvePart1(t *testing.T) {
@@ -36,14 +36,14 @@ func TestSolvePart1(t *testing.T) {
 	}
 }
 
-// func TestSolvePart2(t *testing.T) {
-// 	data, _ := os.ReadFile("testdata/input_2.txt")
+func TestSolvePart2(t *testing.T) {
+	data, _ := os.ReadFile("testdata/input_2.txt")
 
-// 	result := SolvePart2(strings.Split(string(data), "\n"))
+	result := SolvePart2(strings.Split(string(data), "\n"))
 
-// 	expected := int(41_823_587_546)
+	expected := int(41_823_587_546)
 
-// 	if result != expected {
-// 		t.Errorf("Expected %d but got %d", expected, result)
-// 	}
-// }
+	if result != expected {
+		t.Errorf("Expected %d but got %d", expected, result)
+	}
+}
